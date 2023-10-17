9df593c2b395331955cc78916220e28d514dea5d
Aliaksei Molchan <alexeymolchan228@gmail.com>
2023-10-18 01:07:54 +0200

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/game-points-app/",
   plugins: [react()],
   test: {
     environment: "jsdom",
