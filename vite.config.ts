f7cd6f1ac082167374392fdf1a310f97084c9559
Aliaksei Molchan <alexeymolchan228@gmail.com>
2023-10-17 19:35:22 +0200

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/game-points-app/",
   plugins: [react()],
   server: {
     open: true,
